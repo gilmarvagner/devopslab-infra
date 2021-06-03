418cb4b069be494106dbfd58fde31220978cf272
Gilmar Vagner <wwwgilmarvagner@gmail.com>
2021-06-02 21:27:02 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,5 +1,5 @@
 # Cria uma VM no Google Cloud
-resource "global_app_engine_application" "app" {
+resource "google_app_engine_application" "app" {
   project = "lab-devops-cloud-315600"
   location_id = "us-central"
 }
